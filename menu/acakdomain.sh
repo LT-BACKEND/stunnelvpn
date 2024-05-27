8b6d7c4d0a6a9fd3d73c08f3285d2df5084d6c1f
LT-BACKEND <lunaticvpnscript293@gmail.com>
2024-05-27 17:27:56 +0700

    Update acakdomain.sh

--- menu/acakdomain.sh
+++ menu/acakdomain.sh
@@ -53,4 +53,38 @@ domain=$(cat /root/domain)
 cp -r /root/domain /etc/xray/domain
 #read -n 1 -s -r -p "Press any key to back on genssl"
 #genssl
-wget https://raw.githubusercontent.com/LT-BACKEND/stunnelvpn/momok/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
+#wget https://raw.githubusercontent.com/LT-BACKEND/stunnelvpn/momok/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
+clear
+echo -e "\e[44;96;1m     PASANG CERT SSL ( NGINX ) \e[0m"
+echo -e ""
+systemctl stop nginx
+systemctl stop xray
+domain=$(cat /var/lib/scrz-prem/ipvps.conf | cut -d'=' -f2)
+Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
+if [[ ! -z "$Cek" ]]; then
+sleep 1
+echo -e "[ ${red}WARNING${NC} ] Detected port 80 used by $Cek " 
+systemctl stop $Cek
+sleep 2
+echo -e "[ ${GREEN}INFO${NC} ] Processing to stop $Cek " 
+sleep 1
+fi
+echo -e "[ ${GREEN}INFO${NC} ] Starting renew gen-ssl... " 
+sleep 2
+/root/.acme.sh/acme.sh --upgrade
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+echo -e "[ ${GREEN}INFO${NC} ] Renew gen-ssl done... " 
+sleep 2
+echo -e "[ ${GREEN}INFO${NC} ] Starting service $Cek " 
+sleep 2
+echo $domain > /etc/xray/domain
+systemctl start nginx
+systemctl start xray
+echo -e "[ ${GREEN}INFO${NC} ] All finished... " 
+sleep 0.5
+echo ""
+cd
+#read -n 1 -s -r -p "Press any key to back on menu"
